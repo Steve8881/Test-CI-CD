352e6d63953eeeea0d8fa04d06f9678544a08466
Steve <1692933451@qq.com>
2024-11-03 01:36:23 +0800

    go

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from graph_tool import all
+import graph_tool.all as gt
+
+a = np.random.randint(0, 10, (10, 10))
+b = pd.DataFrame()
+plt.show()
+help(gt.Graph)
